163d950d395a4a2da5549e8284a2a9aba06aea83
Blito00 <rodriguezpalo0904@gmail.com>
2023-08-14 16:42:42 -0300

    Volviendo al proyecto 14-08

--- FRONT-END/src/views/Home.jsx
+++ FRONT-END/src/views/Home.jsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
 import { Button } from 'react-native-elements';
 
 
@@ -8,59 +8,63 @@ export default function Home({ navigation }) {
 
     return (
 
-    <ImageBackground
-        source={require('../../assets/img/fondo5.png')}
+        <ImageBackground
+            source={require('../../assets/img/fondo5.png')}
             style={styles.background}>
-
-        <View style={styles.container}>
-            <Text style={styles.text}>Bienvenid@ a Oficios Formosa!</Text>
-            <Text style={styles.text1}>
-                ¡¿Ofrece un oficio?! Ingrese en "Ofrecer".
-            </Text>
-            <Button
-                onPress={() => {navigation.navigate('')}}
-                title="Ofrecer"
-                color="green"
-            />
-            <Text style={styles.text1}>
-                ¡¿Busca un profesional?! Pulse en "Buscar".
-            </Text>
-            <Button
-                onPress={() => {navigation.navigate('login')}}
-                title="Buscar"
-                buttonStyle={styles.button}
-                titleStyle={styles.buttonText}
-            />
-            <StatusBar style="auto  " />
+            <View style={styles.container}>
+            <Image
+                    source={require('../../assets/img/LOGO-AOF2.png')}
+                style={styles.image}/>
+            </View>
+            <View style={styles.container}>
+                
+                <Text style={styles.text}>Bienvenid@ a Oficios Formosa!</Text>
+                <Text style={styles.text1}>
+                    ¡¿Ofrece un oficio?! Ingrese en "Ofrecer".
+                </Text>
+                <Button
+                    onPress={() => { navigation.navigate('login') }}
+                    title="Ofrecer"
+                    color="green"
+                />
+                <Text style={styles.text1}>
+                    ¡¿Busca un profesional?! Pulse en "Buscar".
+                </Text>
+                <Button
+                    onPress={() => { navigation.navigate('login') }}
+                    title="Buscar"
+                    buttonStyle={styles.button}
+                    titleStyle={styles.buttonText}
+                />
+                <StatusBar style="auto  " />
 
             </View>
-    </ImageBackground>
+        </ImageBackground>
 
     );
 }
 const styles = StyleSheet.create({
     container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+
     },
     text: {
-    fontWeight: 'bold',
-    fontSize: 25,
-    color: "#000000",
-    fontFamily: "Roboto",
-    top: -280
+        fontWeight: 'bold',
+        fontSize: 25,
+        color: "#FFFF",
+        fontFamily: "Roboto",
     },
     text1: {
-    fontWeight: 'bold',
-    fontSize: 15,
-    color: "#ffff",
-    fontFamily: "Roboto",
-        },
+        fontWeight: 'bold',
+        fontSize: 15,
+        color: "#ffff",
+        fontFamily: "Roboto",
+    },
     background: {
-    flex: 1,
-    resizeMode: 'cover'
+        flex: 1,
+        resizeMode: 'cover'
     },
     button: {
         backgroundColor: '#237834',
@@ -71,5 +75,10 @@ const styles = StyleSheet.create({
     buttonText: {
         fontSize: 16,
     },
-    });
-    
+    image: {
+        width: 300,
+        height: 300,
+        resizeMode: 'contain', // Ajusta el modo de redimensionamiento de la imagen
+    },
+});
+
--- FRONT-END/src/views/Login.jsx
+++ FRONT-END/src/views/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TextInput, StyleSheet, Image, Text, ImageBackground } from 'react-native';
+import { View, TextInput, StyleSheet, Image, Text, ImageBackground, TouchableWithoutFeedback, Linking } from 'react-native';
 import { Button } from 'react-native-elements';
 import { moderateScale, scale, verticalScale, ScaledSheet } from 'react-native-size-matters';
 
@@ -19,84 +19,109 @@ const Login = () => {
         console.log('Email:', email);
         console.log('Password:', password);
     };
+    const LinkText = ({ text, url }) => {z
+        const handleLinkPress = () => {
+            Linking.openURL(url);
+        };
+    const LinkText = ({ text, url }) => {
+        const handleLinkPress = () => {
+            Linking.openURL(url);
+            };
 
-    return (
-        
+        return (
 
-        <View style={styles.container}>
-            <View style={styles.imageContainer}>
-                <Image source={require('../../assets/img/OFICIOS-FORMOSA.png')} style={styles.img} />
+
+            <View style={styles.container}>
+                <View style={styles.imageContainer}>
+                    <Image source={require('../../assets/img/ISOTIPO-AOF-Negro.png')} style={styles.img} />
+                </View>
+                <View>
+                    <Text style={styles.text}>Ingrese su correo:</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Email"
+                        onChangeText={text => setEmail(text)}
+                        value={email}
+                    />
+                    <Text style={styles.text}>Ingrese su contraseña:</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Contraseña"
+                        onChangeText={text => setPassword(text)}
+                        value={password}
+                        secureTextEntry
+                    />
+                    <Button
+                        title="Ingresar"
+                        onPress={handleLogin}
+                        buttonStyle={styles.button}
+                        titleStyle={styles.buttonText}
+                    />
+                    <Text style={styles.LinkText}>
+                        ¿No tienes una cuenta? Registrate
+                        <LinkText text="enlace" onPress={handleLinkPress} />
+                    </Text>
+                </View>
             </View>
-            <View>
-            <Text style={styles.text}>Ingrese su correo:</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Email"
-                onChangeText={text => setEmail(text)}
-                value={email}
-            />
-            <Text style={styles.text}>Ingrese su contraseña:</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Contraseña"
-                onChangeText={text => setPassword(text)}
-                value={password}
-                secureTextEntry
-            />
-            <Button
-                title="Ingresar"
-                onPress={handleLogin}
-                buttonStyle={styles.button}
-                titleStyle={styles.buttonText}
-            />
-            </View>
-            
-        </View>
-    );
-};
+        );
+    };
 
-const styles = ScaledSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-    input: {
-        height: '80vs',
-        width: '300@s',
-        margin: '10@ms',
-        padding: '10@ms',
-        borderWidth: '0.1@s',
-        borderRadius: 20,
-        backgroundColor: '#FFFF'
-    },
-    button: {
-        backgroundColor: 'black',
-        borderRadius: 16,
-        marginHorizontal: '100@ms',
-        marginBottom: '100@ms'
-    },
-    buttonText: {
-        fontSize: '16@ms',
-        fontWeight: 'bold',
-    },
-    imageContainer: {
-        alignItems: 'center',
-        marginBottom: '-30@ms'
-    },
-    img: {
-        width: '50@s',
-        height: undefined,
-        aspectRatio: 1,
-        resizeMode: 'contain',
-        borderRadius: 20
-    },
-    text: {
-        marginLeft: 20,
-        fontFamily: 'Roboto',
-        fontWeight: 'bold',
-        fontSize: 20,
-    },
-    
-});
-
+    const styles = ScaledSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: 'center',
+        },
+        containerInput: {
+            flex: 1,
+            justifyContent: 'center',
+        },
+        input: {
+            height: '80vs',
+            width: '300@s',
+            margin: '10@ms',
+            marginLeft: '25@s',
+            padding: '10@ms',
+            borderWidth: '0.1@s',
+            borderRadius: 20,
+            backgroundColor: '#FFFF'
+        },
+        button: {
+            backgroundColor: 'black',
+            borderRadius: 16,
+            marginHorizontal: '100@ms',
+            marginBottom: '100@ms'
+        },
+        buttonText: {
+            fontSize: '16@ms',
+            fontWeight: 'bold',
+        },
+        imageContainer: {
+            alignItems: 'center',
+            marginBottom: '10@ms'
+        },
+        img: {
+            width: '200@s',
+            height: undefined,
+            aspectRatio: 1,
+            resizeMode: 'contain',
+            borderRadius: 20,
+            marginTop: '40@ms'
+        },
+        text: {
+            marginLeft: 20,
+            fontFamily: 'Roboto',
+            fontWeight: 'bold',
+            fontSize: 20,
+        },
+        LinkText: {
+            marginLeft: 60,
+            fontFamily: 'Roboto',
+            fontWeight: 'bold',
+            fontSize: 15,
+            color: 'blue', // Cambia el color del enlace según tu diseño
+            textDecorationLine: 'underline', // Subraya el texto para que parezca un enlace
+        },
+    });
+    }
+}
 export default Login;
