2a77892a9a7c855b4a4b7b05ca12e4eca712b198
Blito00 <rodriguezpalo0904@gmail.com>
2023-08-15 16:33:16 -0300

    Configurando pantalla de login 15-08

--- FRONT-END/src/views/Login.jsx
+++ FRONT-END/src/views/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TextInput, StyleSheet, Image, Text, ImageBackground, TouchableWithoutFeedback, Linking } from 'react-native';
+import { View, TextInput, StyleSheet, Image, Text, ImageBackground} from 'react-native';
 import { Button } from 'react-native-elements';
 import { moderateScale, scale, verticalScale, ScaledSheet } from 'react-native-size-matters';
 
@@ -19,21 +19,14 @@ const Login = () => {
         console.log('Email:', email);
         console.log('Password:', password);
     };
-    const LinkText = ({ text, url }) => {z
-        const handleLinkPress = () => {
-            Linking.openURL(url);
-        };
-    const LinkText = ({ text, url }) => {
-        const handleLinkPress = () => {
-            Linking.openURL(url);
-            };
 
         return (
 
 
             <View style={styles.container}>
                 <View style={styles.imageContainer}>
-                    <Image source={require('../../assets/img/ISOTIPO-AOF-Negro.png')} style={styles.img} />
+                    <Text style={styles.text}>Aplicación de Oficios Formosa</Text>
+                    <Image source={require('../../assets/img/ISOTIPO-AOF-BLANCO--2.png')} style={styles.img} />
                 </View>
                 <View>
                     <Text style={styles.text}>Ingrese su correo:</Text>
@@ -59,7 +52,6 @@ const Login = () => {
                     />
                     <Text style={styles.LinkText}>
                         ¿No tienes una cuenta? Registrate
-                        <LinkText text="enlace" onPress={handleLinkPress} />
                     </Text>
                 </View>
             </View>
@@ -70,6 +62,7 @@ const Login = () => {
         container: {
             flex: 1,
             justifyContent: 'center',
+            backgroundColor: '#282830'
         },
         containerInput: {
             flex: 1,
@@ -82,18 +75,20 @@ const Login = () => {
             marginLeft: '25@s',
             padding: '10@ms',
             borderWidth: '0.1@s',
-            borderRadius: 20,
+            borderRadius: 30,
             backgroundColor: '#FFFF'
         },
         button: {
-            backgroundColor: 'black',
+            backgroundColor: '#f7752f',
             borderRadius: 16,
             marginHorizontal: '100@ms',
-            marginBottom: '100@ms'
+            marginBottom: '100@ms',
+            marginTop: '30@ms'
         },
         buttonText: {
             fontSize: '16@ms',
             fontWeight: 'bold',
+            color: 'black'
         },
         imageContainer: {
             alignItems: 'center',
@@ -112,16 +107,16 @@ const Login = () => {
             fontFamily: 'Roboto',
             fontWeight: 'bold',
             fontSize: 20,
+            color: 'white'
         },
         LinkText: {
             marginLeft: 60,
             fontFamily: 'Roboto',
             fontWeight: 'bold',
             fontSize: 15,
-            color: 'blue', // Cambia el color del enlace según tu diseño
+            color: '#5d5dde', // Cambia el color del enlace según tu diseño
             textDecorationLine: 'underline', // Subraya el texto para que parezca un enlace
         },
     });
-    }
-}
+
 export default Login;
